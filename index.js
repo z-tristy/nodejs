7019f982c7f3e90566c4d143ccaf656fa63ff2ee
tristy <tristy@fstln.io>
2023-11-29 19:09:17 +0800

    add ws

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import http from "http";
 const PORT = 3000;
 
-// import { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
+});
+
+const ws = new WebSocketServer({ server })
+ws.on('connection', function connection(socket) {
+  console.log('服务端接收到信号')
+  socket.on('message', (data)=>{
+    console.log(data)
+    socketMessageHandler({ data, ws, socket });
+  });
+
+  socket.send('Socket connected asdasdasdasd');
 });
 
 server.listen(PORT, () => {
