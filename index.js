8d0f4cea9b05a7462a0ff07c51a7f19f3edc626a
tristy <tristy@fstln.io>
2023-11-30 12:06:24 +0800

    添加 express 并且从目标网站将源代码抓取到本地

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import http from "http";
-import { cpSync } from "fs";
+import http from "http"
+import https from "https"
+import ejs from "ejs"
+import express from "express"
+import { cpSync, writeFileSync, readFileSync } from "fs"
 const PORT = 3000;
 
 import { fileURLToPath } from 'url';
@@ -44,11 +47,60 @@ insertAfterLine(
   '@import "../../custom-scss/index";'
 )
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! 1');
-});
+// 创建一个 Express 应用程序实例
+const app = express()
+
+// 修改服务器的路由处理部分，使用 Express 的路由方法（如 app.get）来处理请求和响应：
+app.get('/', (req, res) => {
+  const shopSite = "https://iplayground.myshopify.com"
+
+  https.get(shopSite, {
+    headers: {
+      "content-type": "text/html",
+      // "X-Shopify-Access-Token": token,
+      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+    }
+  }, response => {
+    let data = []
+    response.on('data', chunk => {
+      const buffStr = chunk.toString('utf8')
+      data.push(buffStr)
+    })
+
+    response.on('end', () => {
+      const proxyHtmlPath = path.resolve(__dirname, "./src/views/proxy.html")
+
+      console.log('proxyHtmlPath')
+      console.log(proxyHtmlPath)
+
+      const htmlInit = data.join("")
+      const _html = htmlInit.split("</head>")
+
+      const scriptStr = ``
+
+      _html.splice(1, 0, scriptStr)
+
+      const html = _html.join("")
+
+      writeFileSync(proxyHtmlPath, html);
+
+      res.sendFile(proxyHtmlPath);
+
+      // const template = readFileSync(proxyHtmlPath)
+      // const render = ejs.render(template, { content: data })
+
+      // res.writeHead(200, { 'Content-Type': 'text/html' });
+      // res.write(rendered);
+      // res.end();
+
+    });
+  }).on('error', (err) => {
+    console.error(err)
+  })
+})
+
+// 创建一个 HTTP 服务器实例，并将 Express 应用程序实例作为回调函数传递给 http.createServer 方法：
+const server = http.createServer(app)
 
 const ws = new WebSocketServer({ server })
 ws.on('connection', function connection(socket) {
