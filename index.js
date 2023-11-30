e479d6ce086e0b25ee30f8fa61bdc6e8b9609a9c
tristy <tristy@fstln.io>
2023-11-30 15:20:24 +0800

    add connector debug5

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
 
   https.get(shopSite, {
     headers: {
-      "content-type": "text/html",
+      "content-type": "text/html, application/javascript",
       // "X-Shopify-Access-Token": token,
       "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     }
